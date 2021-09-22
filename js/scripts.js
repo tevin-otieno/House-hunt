33aa13a996581eb9cc76c339d73c54aaade12b61
Brian Ongati <mrbrianomollo@gmail.com>
2021-09-23 02:19:15 +0300

    finish up home page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 
 $(document).ready(function(){
+    $("form#contact_us").submit(function (event) {
+        //event.preventDefault();
+        var name = $("input#MERGE1").val();
+        var email = $("input#MERGE0").val();
+        var message = $("textarea#comment").val();
+        if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+          alert(
+            name +
+              ", we have received your message. Thank you for reaching out to us."
+          );
+        } else {
+          alert("Please ensure you have filled both name and email address!");
+        }
+      });
     
 //insert js code here
 
